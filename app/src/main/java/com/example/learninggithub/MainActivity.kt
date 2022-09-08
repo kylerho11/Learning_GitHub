b33203639beb0c5043a2db4d827ca4f22313570e
kylerho11 <112971781+kylerho11@users.noreply.github.com>
2022-09-08 09:30:17 -0700

    Update MainActivity.kt
    cromulent string

--- app/src/main/java/com/example/learninggithub/MainActivity.kt
+++ app/src/main/java/com/example/learninggithub/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //very important variable
         val x = 10
+        val word = "linus is cromulent"
     }
 }
